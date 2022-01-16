a1a18485de473bc228a43ccafa80467988809d42
Jesse Duffield <jessedduffield@gmail.com>
2022-01-16 14:18:04 +1100

    bloody embedded structs

--- pkg/gui/controllers/submodules_controller.go
+++ pkg/gui/controllers/submodules_controller.go
@@ -14,9 +14,19 @@ import (
 	"github.com/jesseduffield/lazygit/pkg/gui/types"
 )
 
-type SubmodulesController struct {
+// if Go let me do private struct embedding of structs with public fields (which it should)
+// I would just do that. But alas.
+type ControllerCommon struct {
 	*common.Common
 	IGuiCommon
+}
+
+type SubmodulesController struct {
+	// I've said publicly that I'm against single-letter variable names but in this
+	// case I would actually prefer a _zero_ letter variable name in the form of
+	// struct embedding, but Go does not allow hiding public fields in an embedded struct
+	// to the client
+	c                    ControllerCommon
 	enterSubmoduleFn     func(submodule *models.SubmoduleConfig) error
 	getSelectedSubmodule func() *models.SubmoduleConfig
 	git                  *commands.GitCommand
@@ -24,16 +34,14 @@ type SubmodulesController struct {
 }
 
 func NewSubmodulesController(
-	common *common.Common,
-	guiCommon IGuiCommon,
+	c ControllerCommon,
 	enterSubmoduleFn func(submodule *models.SubmoduleConfig) error,
 	git *commands.GitCommand,
 	submodules []*models.SubmoduleConfig,
 	getSelectedSubmodule func() *models.SubmoduleConfig,
 ) *SubmodulesController {
 	return &SubmodulesController{
-		Common:               common,
-		IGuiCommon:           guiCommon,
+		c:                    c,
 		enterSubmoduleFn:     enterSubmoduleFn,
 		git:                  git,
 		submodules:           submodules,
@@ -46,37 +54,37 @@ func (self *SubmodulesController) Keybindings(getKey func(key string) interface{
 		{
 			Key:         getKey(config.Universal.GoInto),
 			Handler:     self.forSubmodule(self.enter),
-			Description: self.Tr.LcEnterSubmodule,
+			Description: self.c.Tr.LcEnterSubmodule,
 		},
 		{
 			Key:         getKey(config.Universal.Remove),
 			Handler:     self.forSubmodule(self.remove),
-			Description: self.Tr.LcRemoveSubmodule,
+			Description: self.c.Tr.LcRemoveSubmodule,
 		},
 		{
 			Key:         getKey(config.Submodules.Update),
 			Handler:     self.forSubmodule(self.update),
-			Description: self.Tr.LcSubmoduleUpdate,
+			Description: self.c.Tr.LcSubmoduleUpdate,
 		},
 		{
 			Key:         getKey(config.Universal.New),
 			Handler:     self.add,
-			Description: self.Tr.LcAddSubmodule,
+			Description: self.c.Tr.LcAddSubmodule,
 		},
 		{
 			Key:         getKey(config.Universal.Edit),
 			Handler:     self.forSubmodule(self.editURL),
-			Description: self.Tr.LcEditSubmoduleUrl,
+			Description: self.c.Tr.LcEditSubmoduleUrl,
 		},
 		{
 			Key:         getKey(config.Submodules.Init),
 			Handler:     self.forSubmodule(self.init),
-			Description: self.Tr.LcInitSubmodule,
+			Description: self.c.Tr.LcInitSubmodule,
 		},
 		{
 			Key:         getKey(config.Submodules.BulkMenu),
 			Handler:     self.openBulkActionsMenu,
-			Description: self.Tr.LcViewBulkSubmoduleOptions,
+			Description: self.c.Tr.LcViewBulkSubmoduleOptions,
 			OpensMenu:   true,
 		},
 	}
@@ -87,28 +95,28 @@ func (self *SubmodulesController) enter(submodule *models.SubmoduleConfig) error
 }
 
 func (self *SubmodulesController) add() error {
-	return self.Prompt(popup.PromptOpts{
-		Title: self.Tr.LcNewSubmoduleUrl,
+	return self.c.Prompt(popup.PromptOpts{
+		Title: self.c.Tr.LcNewSubmoduleUrl,
 		HandleConfirm: func(submoduleUrl string) error {
 			nameSuggestion := filepath.Base(strings.TrimSuffix(submoduleUrl, filepath.Ext(submoduleUrl)))
 
-			return self.Prompt(popup.PromptOpts{
-				Title:          self.Tr.LcNewSubmoduleName,
+			return self.c.Prompt(popup.PromptOpts{
+				Title:          self.c.Tr.LcNewSubmoduleName,
 				InitialContent: nameSuggestion,
 				HandleConfirm: func(submoduleName string) error {
 
-					return self.Prompt(popup.PromptOpts{
-						Title:          self.Tr.LcNewSubmodulePath,
+					return self.c.Prompt(popup.PromptOpts{
+						Title:          self.c.Tr.LcNewSubmodulePath,
 						InitialContent: submoduleName,
 						HandleConfirm: func(submodulePath string) error {
-							return self.WithWaitingStatus(self.Tr.LcAddingSubmoduleStatus, func() error {
-								self.LogAction(self.Tr.Actions.AddSubmodule)
+							return self.c.WithWaitingStatus(self.c.Tr.LcAddingSubmoduleStatus, func() error {
+								self.c.LogAction(self.c.Tr.Actions.AddSubmodule)
 								err := self.git.Submodule.Add(submoduleName, submodulePath, submoduleUrl)
 								if err != nil {
-									_ = self.Error(err)
+									_ = self.c.Error(err)
 								}
 
-								return self.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
+								return self.c.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
 							})
 						},
 					})
@@ -119,76 +127,76 @@ func (self *SubmodulesController) add() error {
 }
 
 func (self *SubmodulesController) editURL(submodule *models.SubmoduleConfig) error {
-	return self.Prompt(popup.PromptOpts{
-		Title:          fmt.Sprintf(self.Tr.LcUpdateSubmoduleUrl, submodule.Name),
+	return self.c.Prompt(popup.PromptOpts{
+		Title:          fmt.Sprintf(self.c.Tr.LcUpdateSubmoduleUrl, submodule.Name),
 		InitialContent: submodule.Url,
 		HandleConfirm: func(newUrl string) error {
-			return self.WithWaitingStatus(self.Tr.LcUpdatingSubmoduleUrlStatus, func() error {
-				self.LogAction(self.Tr.Actions.UpdateSubmoduleUrl)
+			return self.c.WithWaitingStatus(self.c.Tr.LcUpdatingSubmoduleUrlStatus, func() error {
+				self.c.LogAction(self.c.Tr.Actions.UpdateSubmoduleUrl)
 				err := self.git.Submodule.UpdateUrl(submodule.Name, submodule.Path, newUrl)
 				if err != nil {
-					_ = self.Error(err)
+					_ = self.c.Error(err)
 				}
 
-				return self.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
+				return self.c.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
 			})
 		},
 	})
 }
 
 func (self *SubmodulesController) init(submodule *models.SubmoduleConfig) error {
-	return self.WithWaitingStatus(self.Tr.LcInitializingSubmoduleStatus, func() error {
-		self.LogAction(self.Tr.Actions.InitialiseSubmodule)
+	return self.c.WithWaitingStatus(self.c.Tr.LcInitializingSubmoduleStatus, func() error {
+		self.c.LogAction(self.c.Tr.Actions.InitialiseSubmodule)
 		err := self.git.Submodule.Init(submodule.Path)
 		if err != nil {
-			_ = self.Error(err)
+			_ = self.c.Error(err)
 		}
 
-		return self.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
+		return self.c.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
 	})
 }
 
 func (self *SubmodulesController) openBulkActionsMenu() error {
-	return self.Menu(popup.CreateMenuOptions{
-		Title: self.Tr.LcBulkSubmoduleOptions,
+	return self.c.Menu(popup.CreateMenuOptions{
+		Title: self.c.Tr.LcBulkSubmoduleOptions,
 		Items: []*popup.MenuItem{
 			{
-				DisplayStrings: []string{self.Tr.LcBulkInitSubmodules, style.FgGreen.Sprint(self.git.Submodule.BulkInitCmdObj().ToString())},
+				DisplayStrings: []string{self.c.Tr.LcBulkInitSubmodules, style.FgGreen.Sprint(self.git.Submodule.BulkInitCmdObj().ToString())},
 				OnPress: func() error {
-					return self.WithWaitingStatus(self.Tr.LcRunningCommand, func() error {
-						self.LogAction(self.Tr.Actions.BulkInitialiseSubmodules)
+					return self.c.WithWaitingStatus(self.c.Tr.LcRunningCommand, func() error {
+						self.c.LogAction(self.c.Tr.Actions.BulkInitialiseSubmodules)
 						err := self.git.Submodule.BulkInitCmdObj().Run()
 						if err != nil {
-							return self.Error(err)
+							return self.c.Error(err)
 						}
 
-						return self.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
+						return self.c.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
 					})
 				},
 			},
 			{
-				DisplayStrings: []string{self.Tr.LcBulkUpdateSubmodules, style.FgYellow.Sprint(self.git.Submodule.BulkUpdateCmdObj().ToString())},
+				DisplayStrings: []string{self.c.Tr.LcBulkUpdateSubmodules, style.FgYellow.Sprint(self.git.Submodule.BulkUpdateCmdObj().ToString())},
 				OnPress: func() error {
-					return self.WithWaitingStatus(self.Tr.LcRunningCommand, func() error {
-						self.LogAction(self.Tr.Actions.BulkUpdateSubmodules)
+					return self.c.WithWaitingStatus(self.c.Tr.LcRunningCommand, func() error {
+						self.c.LogAction(self.c.Tr.Actions.BulkUpdateSubmodules)
 						if err := self.git.Submodule.BulkUpdateCmdObj().Run(); err != nil {
-							return self.Error(err)
+							return self.c.Error(err)
 						}
 
-						return self.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
+						return self.c.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
 					})
 				},
 			},
 			{
-				DisplayStrings: []string{self.Tr.LcBulkDeinitSubmodules, style.FgRed.Sprint(self.git.Submodule.BulkDeinitCmdObj().ToString())},
+				DisplayStrings: []string{self.c.Tr.LcBulkDeinitSubmodules, style.FgRed.Sprint(self.git.Submodule.BulkDeinitCmdObj().ToString())},
 				OnPress: func() error {
-					return self.WithWaitingStatus(self.Tr.LcRunningCommand, func() error {
-						self.LogAction(self.Tr.Actions.BulkDeinitialiseSubmodules)
+					return self.c.WithWaitingStatus(self.c.Tr.LcRunningCommand, func() error {
+						self.c.LogAction(self.c.Tr.Actions.BulkDeinitialiseSubmodules)
 						if err := self.git.Submodule.BulkDeinitCmdObj().Run(); err != nil {
-							return self.Error(err)
+							return self.c.Error(err)
 						}
 
-						return self.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
+						return self.c.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
 					})
 				},
 			},
@@ -197,28 +205,28 @@ func (self *SubmodulesController) openBulkActionsMenu() error {
 }
 
 func (self *SubmodulesController) update(submodule *models.SubmoduleConfig) error {
-	return self.WithWaitingStatus(self.Tr.LcUpdatingSubmoduleStatus, func() error {
-		self.LogAction(self.Tr.Actions.UpdateSubmodule)
+	return self.c.WithWaitingStatus(self.c.Tr.LcUpdatingSubmoduleStatus, func() error {
+		self.c.LogAction(self.c.Tr.Actions.UpdateSubmodule)
 		err := self.git.Submodule.Update(submodule.Path)
 		if err != nil {
-			_ = self.Error(err)
+			_ = self.c.Error(err)
 		}
 
-		return self.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
+		return self.c.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES}})
 	})
 }
 
 func (self *SubmodulesController) remove(submodule *models.SubmoduleConfig) error {
-	return self.Ask(popup.AskOpts{
-		Title:  self.Tr.RemoveSubmodule,
-		Prompt: fmt.Sprintf(self.Tr.RemoveSubmodulePrompt, submodule.Name),
+	return self.c.Ask(popup.AskOpts{
+		Title:  self.c.Tr.RemoveSubmodule,
+		Prompt: fmt.Sprintf(self.c.Tr.RemoveSubmodulePrompt, submodule.Name),
 		HandleConfirm: func() error {
-			self.LogAction(self.Tr.Actions.RemoveSubmodule)
+			self.c.LogAction(self.c.Tr.Actions.RemoveSubmodule)
 			if err := self.git.Submodule.Delete(submodule); err != nil {
-				return self.Error(err)
+				return self.c.Error(err)
 			}
 
-			return self.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES, types.FILES}})
+			return self.c.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.SUBMODULES, types.FILES}})
 		},
 	})
 }
